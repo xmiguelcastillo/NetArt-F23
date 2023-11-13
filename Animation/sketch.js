461df957b1e195ac74119807dc0390b8994efcab
Miguel <144387659+xmiguelcastillo@users.noreply.github.com>
2023-11-12 23:54:03 -0500

    Update sketch.js

--- Animation/sketch.js
+++ Animation/sketch.js
@@ -11,17 +11,17 @@ function draw() {
   rotate(radians(rotationAngle));
   noFill();
 
-  // Draw ellipses
+ 
   for (let i = 0; i < 50; i++) {
     stroke(map(i, 0, 80, 100, 255), map(i, 0, 80, 100, 255), 255, 100);
     ellipse(0, 0, 200 + i * 3, 200 + i * 3);
   }
 
-  // Draw poem text
+
   textAlign(CENTER, CENTER);
   fill(255);
-  textSize(18);
-  text("In the heart of the Andes, beneath the equatorial gaze, lies Ecuador—a land where ancient mysteries intertwine with the celestial ballet. As night falls, the canvas of stars unfolds, revealing a cosmic poem written above the majestic peaks of Cotopaxi. The cosmic dance echoes from the Amazon to the Pacific shore, a symphony of elements and constellations. Amidst the vastness of space, Ecuador's beauty shines as a celestial law, inviting all to gaze upon the cosmic waltz and embrace the eternal romance between the Andean landscape and the stars.", 0, 0);
+  textSize(10);
+  text("In the heart of the Andes, beneath the equatorial gaze, lies Ecuador—a land where ancient mysteries intertwine with the celestial ballet.", 0, 0);
   
   rotationAngle += random();
 }
